440084b58b572d0ef95f7d7c0c9cc208ba6a8ae8
Vic Jeremy Prajogo <vjrmyp@gmail.com>
2026-01-29 16:34:24 +0700

    first commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,47 @@
+import math
+import sys
+
+# Day 1 - 30DaysOfPython Challenge
+
+# level 1-1
+print(sys.version)
+
+# level 1-2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3%4)
+print(3//4)
+print(3**4)
+
+# level 1-3
+print('Vic Jeremy')
+print('Prajogo')
+print('Indonesia')
+print('I am enjoying 30 days of python')
+
+# level 1-4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Vic Jeremy'))
+print(type('Prajogo'))
+print(type('Indonesia'))
+
+# level 3-1
+print(type(100))
+print(type(6.7))
+print(type(2-1j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type({'name':'Vic'})) 
+print(type({9.8, 3.14, 2.7}))    
+print(type((9.8, 3.14, 2.7)))   
+
+# level 3-2
+# distance between (2, 3) and (10, 8)
+print(math.sqrt(((10-2)**2) + ((8-3)**2)))
+
+
