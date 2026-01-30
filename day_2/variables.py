cb2226f02268e907020d235e907895988c8a3049
Vic Jeremy Prajogo <vjrmyp@gmail.com>
2026-01-30 21:08:52 +0700

    Day 2 done

---
+++ day_2/variables.py
@@ -0,0 +1,53 @@
+# Day 2: 30 Days of python programming
+
+# Exercises: Level 1
+first_name = 'Vic Jeremy'
+last_name = 'Prajogo'
+full_name = 'Vic Jeremy Prajogo'
+country = 'Indonesia'
+city = 'Semarang'
+age = 23
+year = 2026
+is_married, is_true, is_light_on = False, True, True
+
+# Exercises: Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+print(num_one + num_two)
+print(num_one - num_two)
+print(num_one * num_two)
+print(num_one / num_two)
+print(num_one % num_two)
+print(num_one // num_two)
+print(num_one ** num_two)
+
+r = 30
+area_of_circle = 3.14 * r * r
+circum_of_circle = 2 * 3.14 * r
+print(area_of_circle)
+print(circum_of_circle)
+r = int(input('Enter your radius: '))
+print(r)
+print(3.14 * r * r)
+
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+country = input('Enter your country: ')
+age = input('Enter your age: ')
+print(first_name + ' ' + last_name + ' ' + country + ' ' + age)
+
+print(help('keywords'))
